5f502bdeddf655678e90f016537b7e09f346cf60
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-21 16:45:59 +0300

    Додано перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,26 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css'; // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryLightBoxMarkup = createLightBoxImgGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryLightBoxMarkup);
+function createLightBoxImgGallery() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+     `;
+    })
+    .join(``);
+}
+const galleryLightBox = new SimpleLightbox(`.gallery__link`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
+console.log(galleryLightBox);
 console.log(galleryItems);

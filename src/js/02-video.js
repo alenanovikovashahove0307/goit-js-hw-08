2191270eaf8a8942400b659cab90a87f85658050
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-24 18:20:52 +0300

    Довиконано завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,33 +2,29 @@ import "../css/common.css";
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
-const VIDEO_KEY = "videoplayer-current-time";
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
 
-const playStr = function (data) {
-    const dataStr = JSON.stringify(data);
-    localStorage.setItem(VIDEO_KEY, dataStr);
-}
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
 
-player.on('timeupdate', throttle(playStr, 1000),
- function playResult() {
-     if (JSON.parse(localStorage.getItem(VIDEO_KEY)) === null) {
-         return;
-     }
-     const timeSet = JSON.parse(localStorage.getItem(VIDEO_KEY))["seconds"];
-     if (timeSet) {
-         player
-             .setCurrentTime(timeSet)
-             .then(function (seconds) {})
-             .catch(function (error) {
-                 switch (error.name) {
-                     case "Error":
-                        break;
-                 }  
-             })
-       }   
-        
-    });
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-    
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
